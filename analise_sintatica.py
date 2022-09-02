1c1f06ba3a4065f9cbcd27c4eb233a4c61e0f37f
Ivan Pereira <navi1921@gmail.com>
2022-09-02 19:09:24 -0300

    adicionando checagem de sujeito

--- analise_sintatica.py
+++ analise_sintatica.py
@@ -3,24 +3,30 @@ from sentence_split import sentence_split
 from utils import get_num_verbs
 
 
-def syntatic_analysis(x):
-	pass
+def tem_sujeito(doc,nlp):
+	sujeito = [t for t in doc if t.dep_ == "nsubj"]
+	# Checando se tem sujeito. O código final vai ter várias subdivisões dos casos com sujeito e dos casos sem sujeito
+	return sujeito
+
+def syntatic_analysis(doc,nlp):
+	if tem_sujeito(doc,nlp):
+		print('frase: ',doc,' tem sujeito')
 
 #Chamada principal 
 frase='a casa caiu'
 frase='A noiva casa de branco e eu canto músicas.'
 #Bugou denovo(Verificar)
-frase= 'O menino que comprou um carro escreveu um livro.'
+# frase= 'O menino que comprou um carro escreveu um livro.'
 nlp = spacy.load("pt_core_news_lg")
 
 verb_count=get_num_verbs(frase,nlp)
 print(verb_count)
 if verb_count == 0:
-    print('Trata-se de uma frase, não há análise sintática.')
+	print('Trata-se de uma frase, não há análise sintática.')
 elif verb_count == 1:
-	syntatic_analysis(frase)
+	syntatic_analysis(frase,nlp)
 else:
-    oracoes=sentence_split(frase,nlp)
-    for subfrase in oracoes:
-    	syntatic_analysis(subfrase)
+	oracoes=sentence_split(frase,nlp)
+	for subfrase in oracoes:
+		syntatic_analysis(subfrase,nlp)
 
