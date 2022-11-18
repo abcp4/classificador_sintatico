a50eceb83491e10068f463664775972ec7466399
Ricardo Joseh Lima <rjlimauerj@gmail.com>
2022-11-18 06:02:15 -0300

    Situação de sujeito composto

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ def get_num_verbs(frase,nlp):
     verb_count=0
     lemma_list=["canto", "casa", "dúvida", "respeito", "como", "desejo", "morro", "fala", "ajuda"]
     for t in doc:
-        if t.pos_=="VERB" or (t.pos_=="ADJ" and t.dep_ == "conj") or (t.pos_=="ADJ" and t.dep_ in ("ccomp", "xcomp", "ROOT")) or (t.pos_ in ("NOUN") and t.dep_ in ("ccomp", "xcomp", "ROOT", "appos","conj") and t.lemma_ in lemma_list):
+        if t.pos_=="VERB" or (t.pos_=="AUX" and t.dep_ in ("cop", "cop:pass")) or (t.pos_ in ("NOUN") and t.dep_ in ("ccomp", "xcomp", "ROOT", "appos","conj", "amod") and t.lemma_ in lemma_list):
+        # if t.pos_=="VERB" or (t.pos_=="ADJ" and t.dep_ == "conj") or (t.pos_=="ADJ" and t.dep_ in ("ccomp", "xcomp", "ROOT")) or (t.pos_ in ("NOUN") and t.dep_ in ("ccomp", "xcomp", "ROOT", "appos","conj") and t.lemma_ in lemma_list):
             verb_count+=1
     return verb_count
